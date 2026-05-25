5db5f04e2cfa1d805d4e8d5429497c85e8b45436
Nicola Rennie <42338476+nrennie@users.noreply.github.com>
2026-05-25 17:56:58 +0100

    add data

---
+++ posts/freddo-index/R/data.R
@@ -0,0 +1,9 @@
+
+# Packages ----------------------------------------------------------------
+
+library(tidyverse)
+
+
+# Data extracted from https://researchbriefings.files.parliament.uk/documents/CBP-7735/CBP-7735.pdf
+min_wage <- read_csv("posts/freddo-index/data/min_wage.csv")
+
